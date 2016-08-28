b0c29fc62bda87b09dfb47d9c48621a0303c8877
Mohamed El sherif <mohamedalsherif@hotmail.com>
2016-08-29 00:17:15 +0200

    Added first version

---
+++ pages/articles.php
@@ -0,0 +1,16 @@
+<div id="Articles" class="contentbox">
+
+
+
+<div id="articlesDiv" class="centerPseudo">
+
+    <?php
+       $articlesCount=getArticlesCount();
+       for ($i = 0; $i < $articlesCount; $i++)
+       {
+         echo givenArticlesIndexGetArticlesParagraph($i);
+       }
+    ?>
+
+</div>
+</div>
