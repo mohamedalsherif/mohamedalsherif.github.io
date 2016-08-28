1a44fa21adf2f1cbfad7ff28e063548773031ed1
Mohamed El sherif <mohamedalsherif@hotmail.com>
2016-08-29 00:57:20 +0200

    Added 100 percnt width

--- index.php
+++ index.php
@@ -2,28 +2,24 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es" lang="es">
 <?php include_once 'php/header.php';?>
-<body margin: 0pt;>
-  <div id="fb-root"></div>
-<script>(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.5";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script>
-
+<body >
 
 
   <?php include_once 'php/photoSwipe.php';?>
-  <?php include_once 'php/socialNetworks.php';?>
 
   <?php include 'pages/titleInHome.php';?>
-  <div style="text-align: center;
-vertical-align: middle;line-height: 90px; font-size:0.8em;font-weight: lighter;
-    letter-spacing: 0.1em;     font-family: 'Open Sans', sans-serif;" id="tempPage">
-    <p> We are currently in maintenance and will be running again very soon.</p>
-    <p> You can see the <a style="float: none;" href="https://www.facebook.com/Nourine-Hammad-1413192872293705/">Facebook</a>
-      page and follow me on <a style="float: none;" href="https://www.instagram.com/nourinehammadart/">Instagram</a>
+<?php include_once 'php/navigation.php';?>
+  <div id="content" class="visib_hidden">
+
+    <?php include_once 'pages/about.php';?>
+    <?php include_once 'pages/gallery.php';?>
+    <?php include_once 'pages/exhibition.php';?>
+    <?php include_once 'pages/articles.php';?>
+    <?php include_once 'pages/contact.php';?>
   </div>
+
+
+
+
 </body>
 </html>
