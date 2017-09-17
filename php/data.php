295705837c3adf902919793b273f0b103142f879
nourinehammad <mahamadalsharif@gmail.com>
2017-09-17 21:42:41 +0100

    Update data.php

--- php/data.php
+++ php/data.php
@@ -45,6 +45,7 @@ $exhibtionsLocation = "public/img/exhibitions/";
 $exhibitionPhotoSize = "100%";
 $exhibitions= array
   (
+  array("AutumnShow17.jpg","Autumn SHOW","Group show","20 SEPTEMBER - 04 NOVEMBER 2017","PLUS ONE GALLERY - BATTERSEA REACH","http://www.plusonegallery.com/exhibitions/117/overview/"),
    array("WinterShow17.jpg","WINTER SHOW","Group show","24 SEPTEMBER - 25 February 2017","PLUS ONE GALLERY - BATTERSEA REACH","http://www.plusonegallery.com/exhibitions/113/overview/"),
   array("Opening.jpg","OFFICIAL OPENING","Group show","20 SEPTEMBER - 15 OCTOBER 2016","PLUS ONE GALLERY - BATTERSEA REACH","http://www.plusonegallery.com/exhibitions/110/overview/"),
   array("SummerShow.jpg","SUMMER SHOW","Group show","20 JULY - 3 SEPTEMBER 2016","PLUS ONE GALLERY - BATTERSEA REACH","http://www.plusonegallery.com/exhibitions/109/overview/"),
